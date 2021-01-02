2584c2b67230e353e0e94d0355fe84df3b57c125
Ryan <ryhu5281@colorado.edu>
2021-01-02 11:57:18 -0700

    initial commit

---
+++ rescue_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rhughes98/rescue_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rhughes98/rescue_ws/devel;/opt/ros/melodic'.split(';') if '/home/rhughes98/rescue_ws/devel;/opt/ros/melodic' != '' else []
---
+++ rescue_ws/build/rescue_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rhughes98/rescue_ws/src/rescue_pkg
+# Build directory: /home/rhughes98/rescue_ws/build/rescue_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
