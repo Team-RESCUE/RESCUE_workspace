1c0b6609275bbd377d779e2e51d309da7231ad65
RHughes98 <ryan.hughes.md@gmail.com>
2021-03-04 14:44:26 -0700

    meta synch

--- rescue_ws/build/catkin_generated/order_packages.py
+++ rescue_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/ryan/RESCUE_workspace/rescue_ws/src'
 whitelisted_packages = 'rescue_pkg_noetic'.split(';') if 'rescue_pkg_noetic' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/ryan/RESCUE_workspace/rescue_ws/devel;/opt/ros/noetic'.split(';') if '/home/ryan/RESCUE_workspace/rescue_ws/devel;/opt/ros/noetic' != '' else []
