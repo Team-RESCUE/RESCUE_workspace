10fe69a99be4194d2bc6e93d8fe848be1ec7e934
RHughes98 <ryan.hughes.md@gmail.com>
2021-04-12 14:07:36 -0600

    reconfigured for console log testing

--- motor-control/pr_control.py
+++ motor-control/pr_control.py
@@ -1,23 +1,24 @@
-import pigpio
+# import pigpio
 import time
 import numpy as np
 import sys
 
 # define output pins
-pi = pigpio.pi()
+# pi = pigpio.pi()
 pins_out = [13,12]
 # pivot_pinout = 13
 # rot_pinout = 5
-pi.set_mode(pins_out,pigpio.OUTPUT)
+# pi.set_mode(pins_out,pigpio.OUTPUT)
 
 # get initial time
-tick_i = pi.get_current_tick()
+# tick_i = pi.get_current_tick()
 
 # get initial pulsewidths
-this_pws = [pi.get_servo_pulsewidth(pins_out)]
+# this_pws = [pi.get_servo_pulsewidth(pins_out)]
+this_pws = [int(sys.argv[1]), int(sys.argv[2])]
 
 # set target pulsewidths
-target_pws = [int(sys.argv[1]), int(sys.argv[2])]
+target_pws = [int(sys.argv[3]), int(sys.argv[4])]
 
 # pi.set_servo_pulsewidth(pins_out,target_pws)
 
@@ -27,17 +28,19 @@ pw_close = (pw_diff < 20).astype(int)
 
 while np.sum(pw_close) < len(pw_close):
     active = pw_close != 1 # only modify pulsewidth if not close to target
-    pi.set_servo_pulsewidth(pins_out[active], np.add(this_pws[active],target_pws[active])/2)
-    this_pws = [pi.get_servo_pulsewidth(pins_out)]
-    # this_pws = np.add(this_pws,pw_diff)/2
+    # pi.set_servo_pulsewidth(pins_out[active], np.add(this_pws[active],target_pws[active])/2)
+    print(np.add(this_pws[active],target_pws[active])/2)
+    # this_pws = [pi.get_servo_pulsewidth(pins_out)]
+    this_pws = np.add(this_pws,pw_diff/2)
     pw_diff = np.absolute(np.subtract(target_pws,this_pws))
     pw_close = (pw_diff < 20).astype(int)
 
 # final iteration to target pulsewidth
-pi.set_servo_pulsewidth(pins_out,target_pws)
+# pi.set_servo_pulsewidth(pins_out,target_pws)
+print(target_pws)
 
 # get final time
-tick_f = pi.get_current_tick()
+# tick_f = pi.get_current_tick()
 
 # print statements
 print("Pivot pulsewidth changed from ",this_pws[0]," to ",target_pws[0])
